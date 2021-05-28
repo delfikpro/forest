279314e416c47b74a55dfd4ff23fa048372053b1
delfikpro <33707322+DelfikPro@users.noreply.github.com>
2021-05-28 10:47:01 +0300

    Less nagging sourcing checks

--- scripts/executeme.sh
+++ scripts/executeme.sh
@@ -1,10 +1,11 @@
 
-[[ "${BASH_SOURCE[0]}" != "${0}" ]] || (printf "\nPlease run this script via source command:\n source executeme.sh\n\n" && exit 1)
-
 FOREST_HOME_NEW="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 chmod +x "$FOREST_HOME_NEW/forest"
 chmod +x "$FOREST_HOME_NEW/berry"
+
+mkdir "$FOREST_HOME_NEW/resources-bin"
+mkdir "$FOREST_HOME_NEW/resources-text"
 
 echo "export FOREST_HOME=$FOREST_HOME_NEW" >> ~/.bashrc
 echo "export PATH=\$PATH:$FOREST_HOME_NEW" >> ~/.bashrc
@@ -12,4 +13,6 @@ echo "export PATH=\$PATH:$FOREST_HOME_NEW" >> ~/.bashrc
 # shellcheck source=/dev/null
 source ~/.bashrc
 
-rm "$0"
+[[ "${BASH_SOURCE[0]}" != "${0}" ]] || printf "\nForest configured. Please run\n    source ~/.bashrc\nor restart your shell to see changes\n\n"
+
+rm "${BASH_SOURCE[0]}"
